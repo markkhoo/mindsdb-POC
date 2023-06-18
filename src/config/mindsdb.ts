f2753e8755f14cbf046335f829d6b6432d06f032
Mark Khoo <markdkhoo@gmail.com>
2023-06-18 13:19:13 -0700

    ADD adapter functions POC

--- src/config/mindsdb.ts
+++ src/config/mindsdb.ts
@@ -1,30 +1,64 @@
-import MindsDB from 'mindsdb-js-sdk';
-import env from './environment';
+import MindsDB, { ConnectionOptions, Database } from 'mindsdb-js-sdk';
 
 class MindsDBConnector {
-  constructor() {
-    // Initialize any necessary variables or configurations
+  private connectionState: boolean = false;
+  private options: ConnectionOptions
+
+  constructor(
+    options: ConnectionOptions
+  ) {
+    this.options = options;
+    MindsDB.connect(options).then(() => {
+      // connection function does not return success status
+      // so we use a "get all" to check for good connection
+      return MindsDB.Databases.getAllDatabases();
+    }).then(() => {
+      this.connectionState = true;
+      console.log('Connected to MindsDB');
+    }).catch(err => {
+      console.error("FAILED to connect to MindsDB");
+    })
   }
 
-  async connectToMindsDB() {
+  async connect() {
     try {
-      await MindsDB.connect({
-        user: env.MINDSDB_USER,
-        password: env.MINDSDB_PASS,
-      });
+      await MindsDB.connect(this.options);
+      await MindsDB.Databases.getAllDatabases();
+
       console.log('Connected to MindsDB');
+      this.connectionState = true;
+
       return true;
-    } catch (error) {
-      console.error('Failed to connect to MindsDB:', error.message);
+
+    } catch (err) {
+      console.error('Failed to connect to MindsDB:', err);
+      this.connectionState = false;
+
       return false;
     }
   }
 
-  async getDatabase(name: string) {
-    try {
-      return await MindsDB.Databases.getDatabase(name);
-    } catch (error) {
-      console.error('Failed to get database:', error.message);
+  async getAllDatabases(): Promise<[Database[] | null, Error | null]> {
+    if (!this.connectionState) {
+      return [null, Error('Not Connected to MindsDB')]
+    } else {
+      try {
+        if (this.connectionState) return [await MindsDB.Databases.getAllDatabases(), null]
+      } catch (err) {
+        return [null, err]
+      }
+    }
+  }
+
+  async getDatabase(name: string): Promise<[Database | null, Error | null]> {
+    if (!this.connectionState) {
+      return [null, Error('Not Connected to MindsDB')]
+    } else {
+      try {
+        return [await MindsDB.Databases.getDatabase(name), null]
+      } catch (err) {
+        return [null, err]
+      }
     }
   }
 }
--- src/listeners/express.ts
+++ src/listeners/express.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import env from '../config/environment';
 import MindsDBConnector from '../config/mindsdb';
 
 // Create an instance of the MindsDBConnector
-const mindsDB = new MindsDBConnector();
+const mindsDB = new MindsDBConnector({ user: env.MINDSDB_USER, password: env.MINDSDB_PASS });
 const app = express();
 
 app.get('/', (req, res) => {
@@ -17,22 +18,29 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/connect', async (req, res) => {
-  const connection = await mindsDB.connectToMindsDB();
+  const connection = await mindsDB.connect();
 
   if (connection) {
-    res.status(200).json({ status: 'OK', message: 'Connected to MindsDB' });
+    res.status(200).json({ success: true, message: 'Connected to MindsDB' });
   } else {
-    res.status(500).json({ status: 'ERROR', message: 'Failed to connect to MindsDB' });
+    res.status(500).json({ success: false, message: 'Failed to connect to MindsDB' });
   }
 });
 
+app.get('/databases', async (req, res) => {
+  const [databases, error] = await mindsDB.getAllDatabases();
+
+  if (error) res.status(500).json({ success: false, data: error });
+
+  res.status(200).json({ success: true, data: databases });
+})
+
 app.get('/database/:dbname', async (req, res) => {
-  const dbName = req.params.dbname;
-  const database = await mindsDB.getDatabase(dbName);
+  const [database , error] = await mindsDB.getDatabase(req.params.dbname);
 
-  console.log(database);
+  if (error) res.status(500).json({ success: false, data: error });
 
-  res.status(200).json({ status: 'OK', data: database });
+  res.status(200).json({ success: true, data: database });
 });
 
 export default app;
